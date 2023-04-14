88cb856e1133462b9de2add847860d68955c887d
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-04-14 08:35:05 +0300

    Optimised starter code

--- client/constants/index.js
+++
@@ -1,5 +0,0 @@
-import abi from '../backend/contracts/Escrow.sol/Escrow.json'
-
-export const CONTRACT_ADDRESS = '0x01ab1FD0Ee953F304AB59b46BE32E8Bb93E0f70A'
-export const ADMIN_ADDRESS = '0x5cbdf5f9e468df3888e04155668ccafc6f6c4dcf' //0x5cbdf5f9e468df3888e04155668ccafc6f6c4dcf
-export const ABI = abi.abi
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import TimeLine from '../components/TimeLine'
 import { useMoralis, useWeb3Contract } from 'react-moralis'
-import { ABI, ADMIN_ADDRESS, CONTRACT_ADDRESS } from '../../constants'
 
 function Home() {
   const { isWeb3Enabled, account } = useMoralis()
@@ -26,206 +25,33 @@ function Home() {
     '0x5cbdf5f9e468df3888e04155668ccafc6f6c4dcf'
   )
 
-  useEffect(() => {
-    if (isWeb3Enabled) {
-      populateData()
-    }
-  }, [
-    isWeb3Enabled,
-    account,
-    buyerConfirmed,
-    balance,
-    buyerConfirmed,
-    sellerConfirmed,
-  ])
-
-  // Fetch the buyer and seller address
-  const populateData = async () => {
-    try {
-      const returnedBuyer = await getBuyer()
-      setBuyer(returnedBuyer)
-
-      const returnedSeller = await getSeller()
-      setSeller(returnedSeller)
-
-      const buyerStatus = await getBuyerConfirmation()
-      setBuyerConfirmed(buyerStatus)
-
-      const sellerStatus = await getSellerConfirmation()
-      setSellerConfirmed(sellerStatus)
-
-      const balanceResult = await getBalance()
-      setBalance(balanceResult.toString())
-    } catch (error) {
-      console.log(error.response)
-    }
-  }
-
-  const { runContractFunction: getBuyer } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'getBuyer',
-    params: {},
-  })
-
-  //   Get balance
-  const { runContractFunction: getBalance } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'getBalance',
-    params: {},
-  })
-
-  const { runContractFunction: getSeller } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'getSeller',
-    params: {},
-  })
-
-  //   Get buyer Confirmation status
-  const { runContractFunction: getBuyerConfirmation } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'getBuyerConfirmation',
-    params: {},
-  })
-
-  //   Get seller Confirmation status
-  const { runContractFunction: getSellerConfirmation } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'getSellerConfirmation',
-    params: {},
-  })
-
-  //   CONTRACT MUTATION FUNCTIONS
-  //   Deposit money
-  const {
-    isLoading,
-    runContractFunction: deposit,
-    isFetching,
-    error,
-  } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'deposit',
-    params: {},
-    msgValue: 1,
-  })
-
-  //   Buyer Confirm delivery
-  const {
-    // isLoading,
-    runContractFunction: confirmSellerDelivery,
-    // isFetching,
-    // error,
-  } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'confirmSellerDelivery',
-    params: {},
-  })
-
-  //   Seller Confirms receipt
-  const {
-    // isLoading,
-    runContractFunction: confirmBuyerReceipt,
-    // isFetching,
-    // error,
-  } = useWeb3Contract({
-    abi: ABI,
-    contractAddress: CONTRACT_ADDRESS,
-    functionName: 'confirmBuyerReceipt',
-    params: {},
-  })
-
-  const depositFunds = async () => {
-    try {
-      await deposit()
-    } catch (err) {
-      console.log(error)
-    }
-  }
-
-  const confirmDelivery = async () => {
-    try {
-      await confirmSellerDelivery()
-    } catch (err) {
-      console.log(error)
-    }
-  }
-
-  const confirmReceipt = async () => {
-    try {
-      await confirmBuyerReceipt()
-    } catch (err) {
-      console.log(error)
-    }
-  }
-
   return (
     <Layout>
-      <button
-        onClick={depositFunds}
-        className="bg-green-500 p-3 rounded-full px-5 text-white my-12"
-      >
+      <button className="p-3 px-5 my-12 text-white bg-green-500 rounded-full">
         Deposit Payment
       </button>
 
-      {balance && <h1 className="text-xl font-bold">Escrow bal: {balance}</h1>}
+      <h1 className="text-xl font-bold">Escrow bal: </h1>
 
       <h2>
         Admin:{' '}
-        <span className="text-blue-600 cursor-pointer ml-2">
+        <span className="ml-2 text-blue-600 cursor-pointer">
           {`${adminAddress.substring(0, 4)}....${adminAddress.substring(
             adminAddress.length - 4
           )}`}
         </span>
       </h2>
-      <h2>
-        Buyer:
-        {buyer && (
-          <span className="text-blue-600 ml-2 cursor-pointer">
-            {`${buyer.substring(0, 4)}....${buyer.substring(
-              adminAddress.length - 4
-            )}`}
-          </span>
-        )}
-      </h2>
-      <h2>
-        Seller:
-        {seller && (
-          <span className="text-blue-600 ml-2 cursor-pointer">
-            {`${seller.substring(0, 4)}....${seller.substring(
-              adminAddress.length - 4
-            )}`}
-          </span>
-        )}
-      </h2>
+      <h2>Buyer:</h2>
+      <h2>Seller:</h2>
 
       {/* Timeline */}
-      <div className="flex flex-1 items-center">
-        {account &&
-          buyer.toLocaleLowerCase() === account.toLocaleLowerCase() &&
-          !buyerConfirmed && (
-            <button
-              onClick={confirmDelivery}
-              className="bg-blue-300 h-10 rounded-full border-2 border-blue-600 px-4 text-blue-600"
-            >
-              Confirm Delivery
-            </button>
-          )}
-        {account &&
-          seller.toLocaleLowerCase() === account.toLocaleLowerCase() &&
-          !sellerConfirmed && (
-            <button
-              onClick={confirmReceipt}
-              className="bg-orange-300 h-10 rounded-full border-2 border-orange-600 px-4 text-orange-600"
-            >
-              Confirm Recipt
-            </button>
-          )}
+      <div className="flex items-center flex-1">
+        <button className="h-10 px-4 mr-5 text-blue-600 bg-blue-300 border-2 border-blue-600 rounded-full">
+          Confirm Delivery
+        </button>
+        <button className="h-10 px-4 text-orange-600 bg-orange-300 border-2 border-orange-600 rounded-full">
+          Confirm Recipt
+        </button>
       </div>
 
       <TimeLine
