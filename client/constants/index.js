7016f870b0534e0b3d3084d9f8b2ad9de1894fec
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-04-14 08:42:57 +0300

    Added artifacts

--- client/constants/index.js
+++ client/constants/index.js
@@ -1,5 +1,5 @@
 import abi from '../backend/contracts/Escrow.sol/Escrow.json'
 
-export const CONTRACT_ADDRESS = '0x01ab1FD0Ee953F304AB59b46BE32E8Bb93E0f70A'
+export const CONTRACT_ADDRESS = '0x4948531aCaE14d4daDd9F69205d746815D448836'
 export const ADMIN_ADDRESS = '0x5cbdf5f9e468df3888e04155668ccafc6f6c4dcf' //0x5cbdf5f9e468df3888e04155668ccafc6f6c4dcf
 export const ABI = abi.abi
